5bda0194aa4148ff281f548cc5bc032cff1f7fb3
Flanuse <39045674+Flanuse@users.noreply.github.com>
2018-08-21 20:07:06 +0800

    Update UserAuthMiddleware.py

--- fruitshop/utils/UserAuthMiddleware.py
+++ fruitshop/utils/UserAuthMiddleware.py
@@ -29,28 +29,3 @@ class UserMiddleware(MiddlewareMixin):
         else:
             return HttpResponseRedirect(reverse('user:login'))
 
-
-
-
-    # def process_request(self, request):
-    #     paths = ['/adminback/login_back/', '/adminback/register_back/', '/user/login/']
-    #     if request.path in paths:
-    #         return None
-    #     ticket = request.COOKIES.get('ticket')
-    #     if not ticket:
-    #         return HttpResponseRedirect(reverse('adminback:login_back'))
-    #     user_ticket = UserTicketModel.objects.filter(ticket=ticket).first()
-    #
-    #     if user_ticket:
-    #         if datetime.now() > user_ticket.out_time.replace(tzinfo=None):
-    #             user_ticket.delete()
-    #             return HttpResponseRedirect(reverse('adminback:login_back'))
-    #         else:
-    #             request.user = user_ticket.user
-    #             UserTicketModel.objects.filter(Q(user=user_ticket.user) & ~Q(ticket=ticket)).delete()
-    #             return None
-    #     else:
-    #         return HttpResponseRedirect(reverse('adminback:login_back'))
-
-
-# class Middleware(MiddlewareMixin):
